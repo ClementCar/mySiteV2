10d86efae8d98b6d1da471e2038a37fd203641e1
Clement Carcenac <clementcarcenac@gmail.com>
2023-03-29 14:00:41 +0200

    rectif

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -5,21 +5,21 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <!-- Presentation start ============================================================================================= -->
     <section id="presentation" class="section" [ngsRevealSet]="{reset: true}" [ngsSelector]="'.reveal'">
-      <div class="container p-5 reveal">
+      <div class="container-sm reveal pb-5">
         <Div class="text-center py-5 px-4">
           <h3 class="mb-5">Présentation</h3>
           <hr>
           <div class="row row-cols-1 row-cols-lg-2 ">
-            <div class="col reveal">
+            <div class="col mt-4 reveal">
               <p class="fs-3 mt-5">
                 Hey ! Bienvenue sur mon site !<br>
-                Je suis un jeune développeur passionné dans le domaine du Web. Diplômé depuis bientôt un ans, je continue à me former en permanence 
+                Je suis un jeune développeur passionné dans le domaine du Web. Diplômé depuis bientôt un an, je continue à me former en permanence 
                 en particulier sur le framework Angular.
                 Vous trouverez sur ce site tout mon parcours et les projets réalisés.<br>
                 Bonne Navigation !
               </p>
             </div>
-            <div class="col reveal">
+            <div class="col mt-4 reveal">
               <img src="../assets/images/teachpres.svg" alt="présentation">
             </div>
           </div>
